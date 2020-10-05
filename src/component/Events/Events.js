284622771fac86702d0d19f249887519d9bd8a24
Syeda Najah Narjish <najah.masum@gmail.com>
2020-10-05 02:25:09 +0200

    filtering email

--- src/component/Events/Events.js
+++ src/component/Events/Events.js
@@ -3,19 +3,18 @@ import SingleEvent from '../SingleEvent/SingleEvent';
 import { VolunteerContext } from '../../App';
 
 const Events = () => {
-    // const [loggedInUser, setLoggedInUser] = useContext(VolunteerContext);
+    const [loggedInUser, setLoggedInUser] = useContext(VolunteerContext);
 
     const [registeredEvents, setRegisteredEvents] = useState([]);
 
     useEffect(() => {
-        fetch("http://localhost:5000/showRegisteredEvents/")
+        fetch("http://localhost:5000/showRegisteredEvents?email="+ loggedInUser.email)
         .then(res => res.json())
         .then(data => setRegisteredEvents(data))
     }, [registeredEvents])
     return (
         
             <div className="container">
-                {/* <p>{loggedInUser.email}</p> */}
                 <div className="row">
                 {
                     registeredEvents.map(item =><SingleEvent registeredEvent = {item} ></SingleEvent>)
